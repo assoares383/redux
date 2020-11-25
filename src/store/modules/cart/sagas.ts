ab6f0606b20a5aa00d451887fd3f951071d72260
assoares383 <alexandressoares@gmail.com>
2020-11-24 22:31:17 -0300

    configuring redux saga and getting the cart quantity

--- src/store/modules/cart/sagas.ts
+++ src/store/modules/cart/sagas.ts
@@ -1,6 +1,18 @@
-import { all, takeLatest } from 'redux-saga/effects';
+import { IState } from './../../index';
+import { all, takeLatest, select } from 'redux-saga/effects';
+import { addProductToCart } from './actions';
 
-function checkProductStock() {
+type CheckProductStockRequest = ReturnType<typeof addProductToCart>;
+
+function* checkProductStock({payload}: CheckProductStockRequest) {
+  const { product } = payload;
+
+  const currentQuantity: number = yield select((state: IState) => {
+    return state.cart.items.find(item => item.product.id === product.id)?.quantity ?? 0;
+  });
+
+  console.log(currentQuantity);
+
   console.log('Adicionou ao carrinho');
 }
 
